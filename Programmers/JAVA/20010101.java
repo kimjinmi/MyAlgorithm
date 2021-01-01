df6d6f4a55bfc9921da7c8eaa52463998c0f2958
kimjinmi <jinmikim88@gmail.com>
2021-01-01 20:22:07 +0900

    0101 study

---
+++ Programmers/JAVA/20010101.java
@@ -0,0 +1,46 @@
+/*
+0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+
+예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 
+이중 가장 큰 수는 6210입니다.
+
+0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 
+순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+
+제한 사항
+numbers의 길이는 1 이상 100,000 이하입니다.
+numbers의 원소는 0 이상 1,000 이하입니다.
+정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+ */
+
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] stringnum = new String[numbers.length];
+        String answer = "";
+        
+        for(int i=0;i<numbers.length;i++){
+            stringnum[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(stringnum, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+                
+				return ((o2 + o1).compareTo(o1 + o2));
+			}
+            
+		});
+        
+        for(String num : stringnum){
+            answer += num;    
+        }
+        
+        if(stringnum[0].equals("0")){
+            answer = "0";
+        }
+       
+        return answer;
+    }
+}
